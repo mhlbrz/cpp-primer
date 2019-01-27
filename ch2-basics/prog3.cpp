fd548339b0ab3cad6330c6b9243a505d880e6296
Mikhail B <mikhail.berezovskiy@gmail.com>
2019-01-26 22:41:55 -0500

    ch2.2

---
+++ ch2-basics/prog3.cpp
@@ -0,0 +1,8 @@
+int main()
+{
+	//int ival = 1.01; // warning convesion float to int
+	//int &rval = 1.01; // must be an object
+	//int &rval2 = ival;
+	//int &rval3; // reference must be initialized to an object
+	return 0;
+}
