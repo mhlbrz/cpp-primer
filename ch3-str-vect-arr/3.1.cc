77a12fdc43a0a2a2eb072e0b4755ad068dbc77e9
Mikhail <mikhail.b@skrumble.com>
2019-02-26 09:58:03 -0500

    start ch3

---
+++ ch3-str-vect-arr/3.1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+  int sum = 0, val = 1;
+
+  while (val <= 10) {
+    sum += val;
+    ++val;
+  }
+  cout << "Sum of 1 to 10 inclusive is " << sum << endl;
+
+  return 0;
+}
